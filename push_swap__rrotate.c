7a1de484654f48cc3708202a5e3265e15497a849
Octave Graf <cobra.morilles-0n@icloud.com>
2025-07-14 12:30:54 +0200

    rotate and rrotate have now separated files

--- push_swap__rrotate.c
+++ push_swap__rrotate.c
@@ -1,28 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap__rotate.c                                :+:      :+:    :+:   */
+/*   push_swap__rrotate.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ocgraf <ocgraf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 16:57:32 by ocgraf            #+#    #+#             */
-/*   Updated: 2025/07/14 11:25:31 by ocgraf           ###   ########.fr       */
+/*   Updated: 2025/07/14 11:25:24 by ocgraf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-t_stack	*ft_rotate(t_stack *stack)
+t_stack	*ft_rrotate(t_stack *stack)
 {
 	t_stack		*result;
+	t_stack		*temp;
+	t_stack		*temp2;
 
 	if (!stack)
 		return (stack_free(stack), NULL);
-	result = stack_add_back(stack, stack->nb);
+	temp = stack;
+	while (temp->next)
+	{
+		temp2 = temp;
+		temp = temp->next;
+	}
+	result = stack_add_front(stack, temp->nb);
 	if (!result)
 		return (stack_free(stack), NULL);
-	result = result->next;
-	stack_delete(stack);
+	stack_delete(temp);
 	recalculate(result);
+	temp2->next = NULL;
 	return (result);
 }
